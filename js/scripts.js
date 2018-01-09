a10b23ceb46bba67486b48c2d4d0d87bd55cdccd
Alexander Neumann and Christina Thiphavong <christina@>
2018-01-09 15:54:58 -0800

    Add a randomizer button. Still need to increase its refresh-rate.

--- js/scripts.js
+++ js/scripts.js
@@ -10,26 +10,48 @@ $(function(){
       $(this).children("p").fadeToggle();
       $(this).children("img").fadeToggle();
       $(this).children("h3").fadeToggle();
-      console.log(buttonTracker);
     }
   });
 
-$("button#orange").click(function(){
+  $("button#orange").click(function(){
    $("body").removeClass();
    $("body").addClass("orange-background");
-});
+  });
 
-$("button#blue").click(function(){
-  $("body").removeClass();
-  $("body").addClass("blue-background");
-});
+  $("button#blue").click(function(){
+    $("body").removeClass();
+    $("body").addClass("blue-background");
+  });
 
-$("button#toggleAct").click(function(){
+  $("button#toggleAct").click(function(){
   if (buttonTracker === 0){
     buttonTracker = 1;
   }
-  else{
-    buttonTracker = 0;
-  }
-});
+    else {
+      buttonTracker = 0;
+    }
+  });
+  $("button#rave").click(function(){
+    for(i = 1; i <= 10000; i ++){
+    //  $("body").css('background-color', generateThreeRand());
+      first=generateNumberAndHex();
+      second=generateNumberAndHex();
+      third=generateNumberAndHex();
+      rgb="#"+first+second+third;
+      rgb2="#"+ second+first+third;
+      rgb3="#"+ third+first+second;
+      console.log(rgb);
+      $("body").css('background-color', rgb);
+      $(".jumbotron").css('background-color', rgb2);
+      $("h1").css('color', rgb3);
+
+    }
+  });
+  var generateNumberAndHex = function() {
+     rand = Math.floor(Math.random()*155 + 100);
+     stringRand = rand.toString(16);
+     returnRand =stringRand;
+     return returnRand;
+  };
+
 });
