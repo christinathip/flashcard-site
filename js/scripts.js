95f2956212dd656640e9e0c9ec89d8fbde47a6e2
Alexander Neumann and Christina Thiphavong <christina@>
2018-01-09 14:14:31 -0800

    Add more rows and background themes and change toggle activity

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,35 @@
+var buttonTracker = 0;
 $(function(){
+  $(".col-md-2").click(function(){
+    if (buttonTracker === 1){
+      $(this).children("p").toggle();
+      $(this).children("img").toggle();
+      $(this).children("h3").toggle();
+    }
+    else{
+      $(this).children("p").fadeToggle();
+      $(this).children("img").fadeToggle();
+      $(this).children("h3").fadeToggle();
+      console.log(buttonTracker);
+    }
+  });
 
-  $(".col-md-2:first-child").click(function() {
-      $(".col-md-2:first-child h3").toggle();
-      $(".col-md-2:first-child img").toggle();
-      $(".col-md-2:first-child p").toggle();
-  });
-  $(".col-md-2:nth-child(2)").click(function() {
-      $(".col-md-2:nth-child(2) h3").toggle();
-      $(".col-md-2:nth-child(2) img").toggle();
-      $(".col-md-2:nth-child(2) p").toggle();
-  });
-  $(".col-md-2:nth-child(3)").click(function() {
-      $(".col-md-2:nth-child(3) h3").toggle();
-      $(".col-md-2:nth-child(3) img").toggle();
-      $(".col-md-2:nth-child(3) p").toggle();
-  });
-  $(".col-md-2:nth-child(4)").click(function() {
-      $(".col-md-2:nth-child(4) h3").toggle();
-      $(".col-md-2:nth-child(4) img").toggle();
-      $(".col-md-2:nth-child(4) p").toggle();
-  });
-  $(".col-md-2:nth-child(5)").click(function() {
-      $(".col-md-2:nth-child(5) h3").toggle();
-      $(".col-md-2:nth-child(5) img").toggle();
-      $(".col-md-2:nth-child(5) p").toggle();
-  });
-  $(".col-md-2:last-child").click(function() {
-      $(".col-md-2:last-child h3").toggle();
-      $(".col-md-2:last-child img").toggle();
-      $(".col-md-2:last-child p").toggle();
-  });
-  $("#toggleChange").onClick(function(){
-    $("body").removeClass();
-    $("body").addClass("orange-background");
-  });
+$("button#orange").click(function(){
+   $("body").removeClass();
+   $("body").addClass("orange-background");
+});
+
+$("button#blue").click(function(){
+  $("body").removeClass();
+  $("body").addClass("blue-background");
+});
+
+$("button#toggleAct").click(function(){
+  if (buttonTracker === 0){
+    buttonTracker = 1;
+  }
+  else{
+    buttonTracker = 0;
+  }
+});
 });
